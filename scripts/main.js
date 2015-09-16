de7f69f5fd0a0bd6e650db81ea3ef2394adbc85d
Pierre Laferriere <polaferriere@gmail.com>
2015-09-16 17:01:46 -0400

    Added some functionality to the calendar.  The functions break quickly after more than one month is paged.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var monthData = [
     days: 31
   },
   {
-    month: 'Febrary',
+    month: 'February',
     days: 28
   },
   {
@@ -61,6 +61,8 @@ $('.arrow-right').on('click', moveMonthUp);
 function moveMonthUp() {
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   currentMonth = months.indexOf($('.month').text());
+  transferLasttWeekToFirst();
+  populateDates();
   if (currentMonth === 11) {
     moveYearUp();
     $('.month').text('January');
@@ -73,6 +75,8 @@ function moveMonthUp() {
 function moveMonthDown() {
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   currentMonth = months.indexOf($('.month').text());
+  transferFirstWeekToLast();
+  populateDates();
   if (currentMonth === 0) {
     moveYearDown();
     $('.month').text('December');
@@ -92,4 +96,74 @@ function moveYearDown() {
   currentYear = Number($('.year').text());
   currentYear--;
   $('.year').text(currentYear);
+}
+
+/*
+Want to populate cells with dates
+*/
+function populateDates() {
+  var monthDaysNumber = monthData[findWithAttr(monthData, 'month', $('.month').text())].days;
+  for (i = 1; i < monthDaysNumber + 1; i++){
+    $('.date-' + String(i+findLastDay())).text(i);
+  }
+}
+
+function findWithAttr(array, attr, value) {
+   for(var i = 0; i < array.length; i += 1) {
+       if(array[i][attr] === value) {
+           return i;
+       }
+   }
+}
+
+function transferFirstWeekToLast() {
+  for (var i =1; i < 8; i++) {
+    $('.last-week-' + String(i)).text($('.first-week-' + String(i)).text());
+    if (Number($('.last-week-' + String(i)).text()) < 15) {
+      $('li.last-week-' + String(i)).addClass('.shadowed');
+    } else {
+      $('li.last-week-' + String(i)).removeClass('.shadowed');
+    }
+  }
+}
+
+function transferLasttWeekToFirst() {
+  for (var i =1; i < 8; i++) {
+    $('.first-week-' + String(i)).text($('.last-week-' + String(i)).text());
+    if (Number($('.first-week-' + String(i)).text()) > 15) {
+      $('li.first-week-' + String(i)).addClass('shadowed');
+    } else {
+        $('li.last-week-' + String(i)).removeClass('.shadowed');
+    }
+  }
+}
+
+//Gives the number of the date with the last day from the last month
+function findLastDay() {
+  var lastDay = createCurrentMonthArray().indexOf(String(findLastMonthDays()));
+  if (lastDay > 15){
+    return 0;
+  } else {
+  return lastDay+1;
+}
+}
+
+
+//Gives the number of days from the last month
+function findLastMonthDays() {
+  // debugger;
+  var currentMonth = $('.month').text();
+  if (currentMonth === 'January') {
+    return 31;
+  } else {
+    return monthData[(findWithAttr(monthData, 'month', $('.month').text()))-1].days;
+  }
+}
+
+function createCurrentMonthArray() {
+  var currentMonthArray = [];
+  for (var i = 0; i < 35; i++) {
+    currentMonthArray[i] = $('.date-' + String(i+1)).text();
+  }
+  return currentMonthArray;
 }
